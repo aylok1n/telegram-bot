f85880363db8767ee356ce39d3757c8bc5debc13
aylok1n <nikolya.blog@gmail.com>
2021-12-12 17:54:59 +0400

    Add anekdot

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const token = "5033353925:AAFlUfxwGZE99OOjJR_N__oGHyntS_ANL7E"
 
 const bot = new TelegramApi(token, { polling: true })
 
+const fetch = require('node-fetch');
+
 const start = () => {
     bot.setMyCommands([
         {
@@ -22,23 +24,51 @@ const start = () => {
             command: '/xyu',
             description: 'лол чел'
         },
+        {
+            command: '/anekdot',
+            description: 'смешное'
+        },
+        {
+            command: '/porn',
+            description: 'оооууууу май'
+        }
     ])
 
     bot.on('message', async msg => {
         console.log(msg)
         const text = msg.text
         const chatId = msg.chat.id
-        await bot.sendMessage('1305942110', `отправитель @${msg.from.username}\n текст: ${text}`)
-        if (text === "/start") {
-            await bot.sendSticker(chatId, './assets/sticker.webp')
-            bot.sendMessage(chatId, `ты еблан? `)
-        }
-        else if (text === "/inst") bot.sendMessage(chatId, '<a href="https://www.instagram.com/aylok1n/">aylok1n</a>', { parse_mode: 'HTML' })
-        else if (text === "/chiril") bot.sendVoice(chatId, './assets/chiril.mp3')
-        else if (text === "/xyu") bot.sendPhoto(chatId, './assets/xyu.jpg')
-        else if (text === "порно" || text === "Порно") bot.sendPhoto(chatId, './assets/porn.jpg')
-        else {
-            await bot.sendMessage(chatId, `нихуясе и нахуя мне твое ${text}`)
+        await bot.sendMessage('1305942110', `отправитель @${msg.from.username}\nтекст: ${text}`)
+        switch (text) {
+            case "/start":
+                await bot.sendSticker(chatId, './assets/sticker.webp')
+                await bot.sendMessage(chatId, `ты еблан? `)
+                break
+            case "/inst":
+                await bot.sendMessage(chatId, '<a href="https://www.instagram.com/aylok1n/">aylok1n</a>', { parse_mode: 'HTML' })
+                break
+            case "/xyu":
+                await bot.sendPhoto(chatId, './assets/xyu.jpg')
+                break
+            case "/chiril":
+                await bot.sendVoice(chatId, './assets/chiril.mp3')
+                break
+            case "/porn":
+                await bot.sendPhoto(chatId, './assets/porn.jpg')
+                break
+            case "/anekdot":
+                const resp = await fetch('http://umorili.herokuapp.com/api/get?site=bash.im&name=bash&num=100')
+                const res = await resp.json()
+                let anekdot = res[Math.floor(Math.random() * res.length)]
+                try {
+                    bot.sendMessage(chatId, `${anekdot.elementPureHtml.replace(/<br\s*[\/]?>/gi, "\n")}`, { parse_mode: 'HTML' })
+                } catch (error) {
+                    bot.sendMessage(chatId, 'Пошёл ты нахуй чучело')
+                }
+                break
+            default:
+                await bot.sendMessage(chatId, `нихуясе и нахуя мне твое ${text}`)
+                break
         }
     })
 }
